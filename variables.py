44b0e4589c44f87c8089d5488d8e8f61bbc8ab10
rinatb-lab <rinatb768@gmail.com>
2024-11-28 21:43:12 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_homework=12
+total_Number_of_hours=1.5
+name_of_course='Python'
+Time_on_one_homework=number_of_homework/total_Number_of_hours
+print('Курс:',name_of_course, '; всего задач:',number_of_homework, '; затрачено часов:',total_Number_of_hours, '; среднее время выполнения:',total_Number_of_hours/number_of_homework*60,'мин')

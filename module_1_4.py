f09e4ae3357d72cb2d64b4f6c639fa3b6fb5456c
rinatb-lab <rinatb768@gmail.com>
2024-12-01 13:28:48 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string=input('В какой стране Вы живёте? ') # вводим: Я живу в России
+print('Колличество символов: ',len(my_string))
+print('В нижнем регистре: ',my_string.lower())
+print('В верхнем регистре: ',my_string.upper())
+print('Без пробелов: ',my_string.replace(' ',''))
+print('Первая буква: ',my_string[0])
+print('Последняя буква: ',my_string[-1])

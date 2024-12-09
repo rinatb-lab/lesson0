f220a3f516f2a7e039f8b5b856bf64babba319d5
rinatb-lab <rinatb768@gmail.com>
2024-12-09 23:04:39 +0700

    Add files via upload

---
+++ While.py
@@ -0,0 +1,19 @@
+my_list = [42, 69, 322, 13, 0, 99, 5, 9, 0, 0, 6, 3]
+a = len(my_list)
+while a >0:
+    b = int(my_list.pop(0))
+    if b > 0:
+        print(b)
+        a = len(my_list)
+    else:
+        if b == 0:
+            a = len(my_list)
+        else:break
+
+
+
+
+
+
+
+
